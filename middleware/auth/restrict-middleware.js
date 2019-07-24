90ef42090c4c9cfba4a9236c3237c71f4c8631bc
lowlyHero <e.nunn1009@gmail.com>
2019-07-24 18:50:05 -0400

    Added testing depenencies

--- middleware/auth/restrict-middleware.js
+++ middleware/auth/restrict-middleware.js
@@ -1,7 +1,9 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
     if(req.session && req.session.username) {
         next();
     } else {
-        res.status(401).json({ message: 'Who are you? Are you, you? Are you sure? Who is this \'you\'?' });
+        res.status(401).json({ message: 'Who are you? Are you, you? Are you sure? Who is this \'you\' you speak of? But seriously, whoever you are, you messed up. You didn\'t login. Please go back and login so that you can access all of the amazing things you\'ll have unrestricted access to. See you soon.' });
     }
 };
